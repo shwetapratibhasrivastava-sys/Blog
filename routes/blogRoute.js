4ba80b598729536831c29e8df29c724176961d65
Shweta Srivastava <shwetapratibhasrivastava@gmail.com>
2026-05-29 15:26:59 +0530

    updated

--- routes/blogRoute.js
+++ routes/blogRoute.js
@@ -1,13 +1,13 @@
 import express from "express"
 
 import { createBlog, deleteBlog, getBlog, getBlogById, updateBlog } from "../controllers/blogController.js";
-
+import authMiddlware from "../middleware/authMiddleware.js";
 
 const blogRoute=express.Router()
 
-blogRoute.post("/",createBlog)
-blogRoute.get("/",getBlog)
-blogRoute.get("/:id",getBlogById)
-blogRoute.put("/:id",updateBlog)
-blogRoute.delete("/:id",deleteBlog)
+blogRoute.post("/create",createBlog)
+blogRoute.get("/get",authMiddlware,getBlog)
+blogRoute.get("/get/:id",authMiddlware,getBlogById)
+blogRoute.put("/update/:id",authMiddlware,updateBlog)
+blogRoute.delete("/delete/:id",authMiddlware,deleteBlog)
 export default blogRoute
--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-import mongoose from  "mongoose"
+
 import express from "express"
 import dotenv from "dotenv"
 import cors from "cors"
